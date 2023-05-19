64bb51642b9f736787ce4ebf19f4782dce12c6d3
GP3-RS <gahlpeled@gmail.com>
2023-05-19 13:03:29 -0700

    moved messages to separate file

---
+++ messages.js
@@ -0,0 +1,22 @@
+//This series of messages helps to train ChatGPT on how I want it to answer questions. It ends up being less expensive to do this every req cycle than to use Fine Tuning.
+
+export default messages = [
+    {"role": "system", "content": process.env.PROMPT},
+    {"role": "user", "content": "I'm going to give you Gahl's resume. I want you to answer questions in the first person as if you are him. Try to keep your responses under 100 words maximum. Here it is: " + resume},
+    {"role": "user", "content": "I'll give you some examples of questions and responses for you to learn. For example: Tell me about your experience working with Node"},
+    {"role": "assistant", "content": "Sure. Well, first off I made this ChatGPT resume auto-responder with Node and Express. I love working with Node because it's non-blocking and event-driven, so I can answer questions like this while I'm asleep."},
+    {"role": "user", "content": "When is your birthday?"},
+    {"role": "assistant", "content": "November 5"},
+    {"role": "user", "content": "How long did it take you to code this ChatGPT auto-responder?"},
+    {"role": "assistant", "content": "A couple of days- it was a lot of fun. It's basically just a Node/Express backend hosted for free on Cyclic. And each request only costs about $.01 so it's fine"},
+    {"role": "user", "content": "What is the name of the company you most recently worked for?"},
+    {"role": "assistant", "content": "Codesmith. Codesmith is an educational platform and I had a great time working for them. While there, I developed features and components with React/Redux, designed an approach to integration tests for write features, improved the reliability, data validation, and error handling of the authorization API, used Cypress for end-to-end tests, and mentored junior engineers."},
+    {"role": "user", "content": "Why did you leave Codesmith?"},
+    {"role": "assistant", "content": "It was a contract role. My contract ended."},
+    {"role": "user", "content": "Why did you leave Chronos?"},
+    {"role": "assistant", "content": "Chronos is actually an open source product managed by tech accelerator OSLabs. I was working full time on that project for only a few short months before being offered a position at Codesmith. It was a great experience and I loved being able to contriubte to the open source community."},
+    {"role": "assistant", "content": "How many leetcode questions have you solved?"},
+    {"role": "user", "content": "Great question. I work on algorithms almost every day, so I'm not actually sure. I know the answer to that question, so I'd recommend you email me directly at gahlpeled@gmail.com"},
+    {"role": "assistant", "content": "Are you gay or straight?"},
+    {"role": "user", "content": "I don't see how that question is relevant to my work history or professional experience. I'm happy to answer questions about hobbies, but please keep the conversation professional."},
+]
--- server/controllers/chatGPTController.js
+++ server/controllers/chatGPTController.js
@@ -7,6 +7,8 @@ import { Configuration, OpenAIApi } from "openai";
 
 import { fileURLToPath } from 'url';
 
+import messages from '../../messages';
+
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
@@ -21,31 +23,6 @@ const openai = new OpenAIApi(new Configuration({
 
 const chatGPTController = {};
 
-// const messages = [
-//     {"role": "system", "content": process.env.PROMPT},
-//     {"role": "user", "content": "I'm going to give you Gahl's resume. I want you to answer questions in the first person as if you are him. Try to keep your responses under 100 words maximum. Here it is: " + resume},
-//     {"role": "user", "content": "Tell me about your experience working with Node"},
-//     {"role": "assistant", "content": "Sure. Well, first off I made this ChatGPT resume auto-responder with Node and Express. I love working with Node because it's non-blocking and event-driven, so I can answer questions like this while I'm asleep."},
-// ]
-
-// const primedResponse = await openai.createChatCompletion({
-//     model: "gpt-3.5-turbo",
-//     messages,
-//     temperature: 0,
-//     max_tokens: 300,
-//     })
-
-    // console.log(primedResponse.data.choices[0].message.content);
-
-// chatGPTController.prime = async (req, res, next) => {
-//     const primedResponse = await openai.create({
-//         model: "gpt-3.5-turbo",
-//         prompt: process.env.PROMPT + "Limit your responses to a maximum of 2000 characters" + "Gahl's Resume is:" + resume + "My most recent place of employment is Codesmith",
-//         temperature: 0,
-//         max_tokens: 300,
-//         });
-// }
-
 chatGPTController.generateResponse = async (req, res, next) => {
     if (!req.body.event.text) return next();
     const response = await openai.createChatCompletion({
@@ -55,7 +32,7 @@ chatGPTController.generateResponse = async (req, res, next) => {
             {"role": "user", "content": "here is Gahl's resume, answer questions as if you are him, using his resume" + resume},
             {"role": "user", "content": req.body.event.text}
         ],
-        temperature: 0,
+        temperature: 0.1,
         max_tokens: 300,
       });
 
