1c9700e2bba37fbb91c1a2ccb99f1f94e2ccc927
GP3-RS <gahlpeled@gmail.com>
2023-05-19 12:24:14 -0700

    added url verification middleware

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,6 +5,10 @@ import chatGPTController from '../controllers/chatGPTController.js';
 
 const router = express.Router();
 
+router.post('/', (req, res) => {
+  if (req.body.challenge && req.body.type === "url_verification") return res.status(200).json(req.body.challenge);
+})
+
 router.get('/', slackController.testServer, (req,res) => {
   return res.status(200).json("OK")
 })
