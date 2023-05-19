ffa10ab65e8224faafeb4d402f522e62af3d6883
GP3-RS <gahlpeled@gmail.com>
2023-05-18 18:27:00 -0700

    .

--- server/controllers/slackController.js
+++ server/controllers/slackController.js
@@ -19,7 +19,7 @@ slackController.logMessage = (req, res, next) => {
 }
 
 slackController.filterBotMessages = (req, res, next) => {
-    if (req.body.event.client_msg_id && !req.body.event.bot_id === undefined) return next();
+    if (req.body.event.client_msg_id && req.body.event.bot_id === undefined) return next();
     return next({
         log: "error in slackController.filterBotMessages",
         message: { err: 'There was an error with your post request' },
