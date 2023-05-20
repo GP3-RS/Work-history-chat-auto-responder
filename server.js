93ce4a48b7b76c479314a09f0f21869efe24c29a
GP3-RS <gahlpeled@gmail.com>
2023-05-19 20:28:24 -0700

    turning off server

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-import apiRouter from './server/routes/api.js'
-
+import apiRouter from "./server/routes/api.js";
 
 //APP TURNED OFF TO SAVE MONEY DURING TESTING
-app.use('/api', apiRouter);
+// app.use('/api', apiRouter);
 
 app.use((req, res) => res.sendStatus(404));
 
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
+    log: "Express error handler caught unknown middleware error",
     status: 400,
-    message: { err: 'An error occurred' },
+    message: { err: "An error occurred" },
   };
   const errorObj = Object.assign(defaultErr, err);
   console.log(errorObj.log);
