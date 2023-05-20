af75de9bf708865fb757b9e74145554fc1f2b9e9
GP3-RS <gahlpeled@gmail.com>
2023-05-20 12:01:15 -0700

    linted

--- server/controllers/slackController.js
+++ server/controllers/slackController.js
@@ -1,57 +1,56 @@
 import fetch from "node-fetch";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
 const slackController = {};
 
 slackController.logMessage = (req, res, next) => {
-    return next();
-}
+  return next();
+};
 
 slackController.urlVerification = (req, res, next) => {
-    console.log('hitting slackController.urlVerification');
-    if (req.body.challenge && req.body.type === "url_verification") return res.status(200).json(req.body.challenge);
-    else return next();
-}
+  console.log("hitting slackController.urlVerification");
+  if (req.body.challenge && req.body.type === "url_verification")
+    return res.status(200).json(req.body.challenge);
+  else return next();
+};
 
 slackController.filterBotMessages = (req, res, next) => {
-    console.log('hitting slackController.filterBotMessages')
-    if (req.body.event.client_msg_id && req.body.event.bot_id === undefined) {
-        res.locals.platform = 'slack';
-        res.locals.question = req.body.event.text;
-        return next();
-    }
-    else {
-        return res.status(200).json('OK');
-    }
+  console.log("hitting slackController.filterBotMessages");
+  if (req.body.event.client_msg_id && req.body.event.bot_id === undefined) {
+    res.locals.platform = "slack";
+    res.locals.question = req.body.event.text;
+    return next();
+  } else {
+    return res.status(200).json("OK");
+  }
 };
 
 slackController.postMessage = (req, res) => {
-    console.log('IN SLACKCONTROLLER POSTMESSAGE. RESPONSE IS: ', res.locals.response)
-    if (!res.locals.response) return;
-    let payload = {
-        channel: process.env.CHANNEL_NAME,
-        text: res.locals.response
-      }
+  if (!res.locals.response) return;
+  let payload = {
+    channel: process.env.CHANNEL_NAME,
+    text: res.locals.response,
+  };
 
   fetch("https://slack.com/api/chat.postMessage", {
     method: "POST",
     body: JSON.stringify(payload),
     headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
-        Accept: "application/json",
+      "Content-Type": "application/json; charset=utf-8",
+      Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+      Accept: "application/json",
     },
-    })
+  })
     .then((res) => {
-        if (!res.ok) {
+      if (!res.ok) {
         throw new Error(`Server error ${res.status}`);
-        }
-        return res.json();
+      }
+      return res.json();
     })
     .catch((error) => {
-        console.log('ERROR IN POSTMESSAGE:', error);
-    })
-}
+      console.log("ERROR IN POSTMESSAGE:", error);
+    });
+};
 
 export default slackController;
